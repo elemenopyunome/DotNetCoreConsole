f1d871cd48fc253fad5b6868c9264ad8857bbb91
elemenopy <elemenopy@DESKTOP-IRVHDKF>
2020-02-28 23:33:12 -0600

    Add project files.

---
+++ DotNetCoreConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DotNetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Deploy this to github
+            ///Running single thread (main thread) on RHEL 8 - EC2
+            using (WebClient w = new WebClient())
+            {
+                
+                for(int x=0;x<10000;x++)
+                {
+                    var json = w.DownloadString("https://www.learningcontainer.com/wp-content/uploads/2019/10/Sample-employee-JSON-data.json");
+                    string jsontoserialize = JsonConvert.SerializeObject(json);
+                    Console.WriteLine(jsontoserialize);
+                    Console.WriteLine(x.ToString());
+                }
+                Console.WriteLine("Done");
+                //Console.ReadLine();
+            }
+            
+        }
+    }
+}
